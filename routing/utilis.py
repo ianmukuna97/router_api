34ab4806ebb7c6b9c9e433635effb2c27958be17
ianmukuna97 <ianmukuna97@gmail.com>
2025-07-09 23:30:48 +0300

    API Key Config

---
+++ routing/utilis.py
@@ -0,0 +1,6 @@
+import openrouteservice
+from django.conf import settings
+import pandas as pd
+import math
+import urllib.parse
+
--- routing/views.py
+++ routing/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
+import requests
+from django.http import JsonResponse
+from .utilis import get_route_data, find_fuel_stops, calculate_fuel_cost
 
 # Create your views here.
